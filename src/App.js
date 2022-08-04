3c59bca91e49c72f38885d008d4853e103eaa5d1
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2022-08-04 13:29:43 +0100

    html form created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello world</header>
+      <h1>Dictionary</h1>
+      <form className="searchEngine">
+        <input type="search" placeholder="Enter a word..." />
+        <input type="submit" value="Search" />
+      </form>
     </div>
   );
 }
